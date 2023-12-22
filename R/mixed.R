8df791de6dd3d945d1faa3ec17dc9161155d6150
jrpepin <j.pepin@utoronto.ca>
2023-12-22 10:10:14 -0500

    x-axis labels and colors

--- R/mixed.R
+++ R/mixed.R
@@ -16,21 +16,22 @@
 # Variable Processing ----------------------------------------------------------
 group <- c("Visible Minority Group",  "Visible Minority Group",  "Visible Minority Group",  "Visible Minority Group", 
            "Country of Origin", "Country of Origin", "Country of Origin", "Country of Origin")
-type <- c("Both white", "Same\nVisible Minority Group", "White &\nVisible Minority Group", "Different\nVisible Minority Groups",
-           "Both born in Canada", "Born same country\n(not Canada)", "Canada &\nAnother country", "Both born outside\n of Canada")
+type <- c("Both white", "Same Visible\nMinority Group", "White & Visible\nMinority Group", "Different Visible\nMinority Groups",
+           "Both born\nin Canada", "Born same country\n(not Canada)", "Canada &\nAnother country", "Both born outside\nof Canada")
+colors <- c("same", "same", "mixedVMG", "mixedVMG", "same", "same", "mixedOrigin", "mixedOrigin")
 prop <- c(75.7,17,6.7,.6,64.4,19.8,11.8,4.1)
-data <- data.frame(group, type, prop)
+data <- data.frame(group, type, prop, colors)
 
 data$type <- factor(data$type,
-                     levels = c("Both white", "Same\nVisible Minority Group", "White &\nVisible Minority Group", "Different\nVisible Minority Groups",
-                                "Both born in Canada", "Born same country\n(not Canada)", "Canada &\nAnother country", "Both born outside\n of Canada"), ordered = F)
+                     levels = c("Both white", "Same Visible\nMinority Group", "White & Visible\nMinority Group", "Different Visible\nMinority Groups",
+                                "Both born\nin Canada", "Born same country\n(not Canada)", "Canada &\nAnother country", "Both born outside\nof Canada"), ordered = F)
 
 data$group <- factor(data$group, 
                      levels = c("Visible Minority Group", "Country of Origin"), ordered = F)
 
 # Figure -----------------------------------------------------------------------
 
-p <- ggplot(data, aes(x = factor(type), y = prop, fill = group, label = prop)) +
+p <- ggplot(data, aes(x = factor(type), y = prop, fill = colors, label = prop)) +
   geom_col(width = .5) +
   # Set the position to its complementary
   geom_text(show.legend = FALSE, color = "#7b8a8b", vjust=-0.25) +
@@ -39,11 +40,11 @@ p <- ggplot(data, aes(x = factor(type), y = prop, fill = group, label = prop)) +
   theme(legend.position    = "none",
         panel.grid.minor   = element_blank(),
         panel.grid.major.x = element_blank(),
-        axis.text.x        = element_blank(),
         axis.text.y        = element_blank(),
+        axis.text.x        = element_text(angle = 35, vjust = 1.2, hjust=1, size = 12),
         plot.subtitle      = element_text(face = "italic"),
         plot.caption       = element_text(hjust=0, colour = "grey", size = 12)) +
-  scale_fill_manual(values = c_palette) +
+  scale_fill_manual(values = c("#3498DB",  "#20c997", "#b4bcc2")) +
   labs(x        = NULL, 
        y        = NULL,
        fill     = NULL,
